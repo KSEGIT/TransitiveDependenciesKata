41588050305b143d46af1c8b8dc3afca4b1a6094
Daniel Kiska <daniel.kiska@gmail.com>
2022-11-22 11:04:21 +0000

    Upgrade and clean up

--- TransitiveDependenciesKata.cpp
+++ TransitiveDependenciesKata.cpp
@@ -1,5 +1,5 @@
 /*
-Transitive Dependecies Kata
+Transitive Dependencies Kata
 author: Daniel Kiska
 date: 16.11.2022
 */
@@ -15,62 +15,54 @@ char* holder;
 
 class DependenciesGraph {
 private:
-	//map data structure for dependecies
-	std::map <std::string, std::vector<std::string>>* _dependencyMap;
-	//decided to use sets which run O(log n) time vs (vector O(n)) which is much more efficient with large numbers of elements and preventing duplicates when iterating
-	std::set <std::string>* _completelist;
-
+	//map data structure for dependencies
+	std::map <std::string, std::vector<std::string>> _dependencyMap;
+	
 public:
-	DependenciesGraph() {
-		_dependencyMap = new std::map <std::string, std::vector<std::string>>();
-		_completelist = new std::set <std::string>;
-	};
-	~DependenciesGraph() {
-		delete _dependencyMap;
-		delete _completelist;
-	}
-	void addDependency(const std::string& token, std::vector<std::string> dependecies) {
-		_dependencyMap->insert({ token , dependecies });
+	//adding dependencies for token
+	void addDependency(const std::string& token, const std::vector<std::string>& dependencies) {
+		_dependencyMap.insert({ token , dependencies });
 	}
 
 	//printing map for debug
-	void printMap() const {
+	void printMap2() const {
 		std::cout << "mymap contains:\n";
-		for (std::map<std::string, std::vector<std::string>>::iterator it = _dependencyMap->begin(); it != _dependencyMap->end(); ++it) {
-			std::cout << it->first << ": ";
-			std::vector <std::string> inVector = it->second;
-			for (unsigned j = 0; j < inVector.size(); j++) {
-				std::cout << inVector[j] << " ";
+		for (const auto& [key, value] : _dependencyMap) {
+			std::cout << key << ": ";
+			for (const auto& dep : value) {
+				std::cout << dep << " ";
 			}
 			std::cout << std::endl;
 		}
 	}
 
-	// getting direct dependencies and runing iteration on transitive dependencies (returns full dependency list)
+	// getting direct dependencies and running iteration on transitive dependencies (returns full dependency list)
 	std::string tokenDependency(std::string const& token) {
-		//making sure that set is clear
-		_completelist->clear();
-		//adding direct dependecies 
-		std::copy(_dependencyMap->at(token).begin(), _dependencyMap->at(token).end(), std::inserter(*(_completelist), _completelist->end()));
+		//sets which run O(log n) time vs(vector O(n)) which is much more efficient with large numbers of elements and prevents duplicates when iterating
+		std::set <std::string> completelist;
+		//adding direct dependencies 
+		completelist.insert(_dependencyMap[token].begin(), _dependencyMap[token].end());
+
+
 		//scan for transitive dependencies
-		std::set<std::string>::iterator it = _completelist->begin();
-		while (it != _completelist->end())
+		std::set<std::string>::iterator it = completelist.begin();
+		while (it != completelist.end())
 		{
 			//guard to block circular dependency
 			if (*it != token)
 			{
 				//There was memory leak when no "key" found inside map. Call map.at() was forbidding compiler to create key with null value
-				std::copy((*_dependencyMap)[(*it)].begin(), (*_dependencyMap)[(*it)].end(), std::inserter(*(_completelist), _completelist->end()));
+				std::copy(_dependencyMap[(*it)].begin(), _dependencyMap[(*it)].end(), std::inserter(completelist, completelist.end()));
 				++it;
 			}
 			else
 			{
-				return "circular dependency found!";
+				return "Circular dependency found! Provide correct dependencies.";
 			}
 		}
 		//creating dependencies string
-		std::string finalString = "";
-		for (std::string i : *(_completelist))
+		std::string finalString;
+		for (const std::string& i : completelist)
 			finalString += i + " ";
 		return token + ": " + finalString;
 	}
@@ -89,8 +81,7 @@ int main()
 	dataTree.addDependency("D", { "A", "F" });
 	dataTree.addDependency("E", { "F" });
 	dataTree.addDependency("F", { "H" });
-	//dataTree.printMap();
-
+	dataTree.printMap2();
 	std::cout << dataTree.tokenDependency("A") << " \n";
 	std::cout << dataTree.tokenDependency("B") << " \n";
 	std::cout << dataTree.tokenDependency("C") << " \n";
@@ -98,16 +89,21 @@ int main()
 	std::cout << dataTree.tokenDependency("E") << " \n";
 	std::cout << dataTree.tokenDependency("F") << " \n";
 
-	std::cout << "\n =============== Part 2 ===============\n";
+	std::cout << "\n =============== Part 2 =============== \n";
 	static DependenciesGraph dataTree2;
 	dataTree2.addDependency("A", { "B" });
 	dataTree2.addDependency("B", { "C" });
 	dataTree2.addDependency("C", { "A" });
-	//dataTree2.printMap();
-
+	dataTree2.printMap2();
 	std::cout << dataTree2.tokenDependency("A") << " \n";
 	std::cout << dataTree2.tokenDependency("B") << " \n";
 	std::cout << dataTree2.tokenDependency("C") << " \n";
+
+	std::cout << "\n =============== Part 3 =============== \n";
+	dataTree.addDependency("I", { "J" });
+	dataTree.addDependency("J", { "A" });
+	std::cout << dataTree.tokenDependency("I") << " \n";
+	dataTree.printMap2();
 
 	std::cin >> holder;
 	return 0;
